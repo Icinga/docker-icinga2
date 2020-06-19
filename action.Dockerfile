4fecaed2ddf8abd9867d78547656be2ededee9de
Alexander A. Klimov <alexander.klimov@icinga.com>
2020-06-19 10:39:00 +0200

    action.bash: build Icinga 2

--- action.Dockerfile
+++ action.Dockerfile
@@ -1,19 +1,4 @@
-FROM buildpack-deps:scm as clone
-SHELL ["/bin/bash", "-exo", "pipefail", "-c"]
-
-RUN mkdir actions ;\
-	cd actions ;\
-	git clone --bare https://github.com/actions/checkout.git ;\
-	git -C checkout.git archive --prefix=checkout/ v2 |tar -x ;\
-	git clone --bare https://github.com/actions/upload-artifact.git ;\
-	git -C upload-artifact.git archive --prefix=upload-artifact/ v2 |tar -x ;\
-	rm -rf *.git
-
-
-FROM docker
-
-RUN ["apk", "add", "bash", "grep", "nodejs"]
-COPY --from=clone /actions /actions
+FROM icinga/icinga2-builder
 
 COPY action.bash Dockerfile /
 
--- action.bash
+++ action.bash
@@ -3,11 +3,40 @@ set -exo pipefail
 
 TARGET=icinga/icinga2
 
+cache () {
+	INPUT_KEY=docker-image/ccache INPUT_PATH=ccache \
+		STATE_CACHE_KEY=1 STATE_CACHE_RESULT=2 \
+		node "/actions/cache/dist/${1}/index.js"
+}
+
 mkimg () {
 	test -n "$TAG"
 
 	node /actions/checkout/dist/index.js |grep -vFe ::add-matcher::
+	cache restore
+
+	export PATH="/usr/lib/ccache:$PATH"
+	mkdir -p ccache
+	ln -vs "$(pwd)/ccache" ~/.ccache
+
+	mkdir icinga2-bin
+	mkdir build
+	cd build
+
+	cmake -DCMAKE_INSTALL_PREFIX=/usr -DCMAKE_INSTALL_SYSCONFDIR=/etc \
+		-DCMAKE_INSTALL_LOCALSTATEDIR=/var -DICINGA2_RUNDIR=/run \
+		-DICINGA2_SYSCONFIGFILE=/etc/sysconfig/icinga2 -DICINGA2_WITH_{COMPAT,LIVESTATUS}=OFF ..
+
+	make
+	make test
+
+	make install "DESTDIR=$(pwd)/../icinga2-bin"
+
+	cd ..
+
+	cache save
 	docker build -f /Dockerfile -t "${TARGET}:$TAG" .
+
 	STATE_isPost=1 node /actions/checkout/dist/index.js
 
 	docker save "${TARGET}:$TAG" |gzip >docker-save.tgz
