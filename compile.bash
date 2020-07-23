6beed164f18c49fa6d782e84b5246a98b7e5bf48
Alexander A. Klimov <alexander.klimov@icinga.com>
2020-07-23 13:18:13 +0200

    Show ccache stats

--- compile.bash
+++ compile.bash
@@ -2,6 +2,7 @@
 set -exo pipefail
 
 export PATH="/usr/lib/ccache:$PATH"
+ccache -z
 
 mkdir icinga2-bin
 mkdir build
@@ -19,3 +20,5 @@ make install "DESTDIR=$(pwd)/../icinga2-bin"
 cd ..
 
 rm icinga2-bin/etc/icinga2/features-enabled/mainlog.conf
+
+ccache -s
