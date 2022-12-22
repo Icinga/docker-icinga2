e9b428ee66e98c92bd530cdbf4b4bfefabea4c3e
Alexander A. Klimov <alexander.klimov@icinga.com>
2022-12-22 17:29:35 +0100

    Don't ship our docs (10MB)

--- compile.bash
+++ compile.bash
@@ -19,6 +19,7 @@ make test
 make install "DESTDIR=$(pwd)/../icinga2-bin"
 strip -g ../icinga2-bin/usr/lib/*/icinga2/sbin/icinga2
 strip -g ../icinga2-bin/usr/lib/nagios/plugins/check_nscp_api
+rm -rf ../icinga2-bin/usr/share/doc/icinga2/markdown
 
 cd ..
 
